aee82f3b1c269065aca5d9aa8337ea45727c9ffd
James777G <hgon777@aucklanduni.ac.nz>
2023-06-27 16:12:37 +1200

    Screen Control

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import { About } from './page/homePage';
 import './styles/base/Base.css'
+import React, { useState, useEffect } from 'react';
+
+// Custom hook to track window width
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowWidth;
+};
+
+const ErrorMessage = () => (
+  <p style={{color: 'black', backgroundColor: 'white'}}>
+    This website is best viewed on a laptop or desktop screen
+  </p>
+);
 
 function App() {
+  const windowWidth = useWindowWidth();
   return (
     <div className="App">
-      <About />
+      {windowWidth >= 1000 ? <About /> : ErrorMessage()}
     </div>
   );
 }
